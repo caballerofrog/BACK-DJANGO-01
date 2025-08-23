a98b4a265e09bc6d4741f603c737cbac96b52b3d
caballerofrog <jhoelegus357@gmail.com>
2025-08-22 21:50:03 -0500

    whitenoise

--- ProjectTienda/settings.py
+++ ProjectTienda/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", ## gregado
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -112,13 +113,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
+
+## * para guardar imgenes css img js
+STATIC_URL = '/static/'
+## * va junto con el anterior
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'AppHome/static')
     ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+## * pra despliegue y whitenoise
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
+## *para imgen
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
